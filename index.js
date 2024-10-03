0a7fd90324f7e8ccc6adf6109a9c8782433ab438
datasantana <juan.linares@geostudio.com.co>
2024-10-03 10:43:41 -0500

    Refactor CORS origin check in index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const routerAPI = require('./routes');
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/errorHandler.js')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 const whitelist = ['http://localhost:8080']; // List of allowed domains. To add all domains, use ['*']
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
